d2e8249d2fb92a5851677a347b0b3eecdf5b04a4
PowderLi <min.li@zilliz.com>
2024-04-13 12:59:03 +0800

    add test
    Signed-off-by: PowderLi <min.li@zilliz.com>

---
+++ internal/util/grpcclient/datadog.go
@@ -0,0 +1,80 @@
+// Licensed to the LF AI & Data foundation under one
+// or more contributor license agreements. See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership. The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License. You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// grpc zstd implementation from https://github.com/cortexproject/cortex
+
+package grpcclient
+
+import (
+	"bytes"
+	"io"
+
+	"github.com/DataDog/zstd" // Import Datadog's zstd package
+	"google.golang.org/grpc/encoding"
+)
+
+const (
+	Datadog = "datadog"
+)
+
+type datadogCompressor struct{}
+
+func init() {
+	c := &datadogCompressor{}
+	encoding.RegisterCompressor(c)
+}
+
+func (c *datadogCompressor) Compress(w io.Writer) (io.WriteCloser, error) {
+	return &datadogWriteCloser{
+		writer: w,
+	}, nil
+}
+
+type datadogWriteCloser struct {
+	writer io.Writer    // Compressed data will be written here.
+	buf    bytes.Buffer // Buffer uncompressed data here, compress on Close.
+}
+
+func (z *datadogWriteCloser) Write(p []byte) (int, error) {
+	return z.buf.Write(p)
+}
+
+func (z *datadogWriteCloser) Close() error {
+	// prefer faster compression decompression rather than compression ratio
+	compressed, err := zstd.CompressLevel(nil, z.buf.Bytes(), 3)
+	if err != nil {
+		return err
+	}
+	_, err = z.writer.Write(compressed)
+	return err
+}
+
+func (c *datadogCompressor) Decompress(r io.Reader) (io.Reader, error) {
+	compressed, err := io.ReadAll(r)
+	if err != nil {
+		return nil, err
+	}
+
+	// Use Datadog's API to decompress data
+	uncompressed, err := zstd.Decompress(nil, compressed)
+	if err != nil {
+		return nil, err
+	}
+	return bytes.NewReader(uncompressed), nil
+}
+
+func (c *datadogCompressor) Name() string {
+	return Datadog
+}
